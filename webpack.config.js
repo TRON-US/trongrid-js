6cb510c08d7308e3f83cbd166d45ac99731a5be4
Francesco Sullo <sullof@gmail.com>
2019-02-27 17:28:57 -0800

    Minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = [
         ...baseConfig,
         output: {
             path: path.resolve(__dirname, 'dist'),
-            filename: 'TronGrid.node.js',
+            filename: 'trongrid.node.js',
             libraryTarget: 'commonjs2',
             libraryExport: 'default'
         },
@@ -92,8 +92,8 @@ module.exports = [
         ...baseConfig,
         output: {
             path: path.resolve(__dirname, 'dist'),
-            filename: 'TronGrid.js',
-            library: 'TronGrid',
+            filename: 'trongrid.js',
+            library: 'trongrid',
             libraryTarget: 'umd',
             libraryExport: 'default',
             umdNamedDefine: true
