aee3e6fb04b303ebd3ee87f69a2277ac9edab790
Francesco Sullo <sullof@gmail.com>
2019-03-06 10:54:38 -0800

    Implementing a plugin interface for TronWeb

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-var utils
-
 import Account from 'lib/Account';
 import Asset from 'lib/Asset'
 
@@ -10,7 +8,27 @@ class TronGrid {
             throw new Error('Expected instance of TronWeb');
 
         this.tronWeb = tronWeb;
+        this.utils = tronWeb.utils
         this.account = new Account(this);
         this.asset = new Asset(this);
+    }
+
+    pluginInterface() {
+        let self = this
+        return {
+            requires: '^2.2.2',
+            components: {
+                trx: {
+                    getTransactionsToAddress:
+                        (address = this.tronWeb.defaultAddress.hex, limit = 30, offset = 0, callback = false) => {
+                            // TODO this should read the result and return only the data array
+                            return self.account.getTransactions(address, {
+                                size: limit
+                            }, callback)
+                        }
+                }
+            }
+        }
+
     }
 }
