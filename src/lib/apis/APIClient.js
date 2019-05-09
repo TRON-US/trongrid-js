ef9945b47db6c07958116404c6d65d602dbb8b60
shenyongri <shenyongri110@gmail.com>
2019-05-08 18:39:16 -0700

    Minor change

--- src/lib/apis/APIClient.js
+++ src/lib/apis/APIClient.js
@@ -4,23 +4,12 @@ export default class APIClient {
     constructor(tronGrid) {
         this.tronGrid = tronGrid;
         this.apiNode = tronGrid.tronWeb.eventServer;
-        this.authWhitelist = [
-            'v1/accounts',
-            'v1/assets',
-            'v1/blocks',
-            'v1/contracts',
-            'v1/transactions'
-        ];
-        this.authBlacklist = [];
     }
 
     _httpClient(path, options, callback, method = 'get') {
 
-        const pathPrefix = path.slice(0, path.indexOf('/', 3));
-        if (this.tronGrid.experimental) {
+        if (!options.experimental && this.tronGrid.experimental) {
             options.experimental = this.tronGrid.experimental;
-        } else if (this.authWhitelist.indexOf(pathPrefix) === -1) {
-            throw new Error('This API requires an experimental code.');
         }
 
         this.apiNode.request(path, options, method).then(response => {
