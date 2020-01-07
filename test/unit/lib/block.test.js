6e25c1a17514dd8403e7e31a48b33fe5766eb11c
Francesco Sullo <sullof@gmail.com>
2020-01-07 11:33:34 -0800

    Fix tests

--- test/unit/lib/block.test.js
+++ test/unit/lib/block.test.js
@@ -1,7 +1,7 @@
 const {assert, tronGridBuilder, net} = require('../../helpers/includes')
 
 
-describe.only('#block functional unit test', function () {
+describe('#block functional unit test', function () {
 
     let tronGrid;
 
